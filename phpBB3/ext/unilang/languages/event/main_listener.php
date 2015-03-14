042c051532da9103e3183d6829cdc405b36352cf
Maarten van Gompel <proycon@anaproy.nl>
2015-03-14 23:40:40 +0100

    added hook core.memberlist_view_profile

--- phpBB3/ext/unilang/languages/event/main_listener.php
+++ phpBB3/ext/unilang/languages/event/main_listener.php
@@ -22,7 +22,9 @@ class main_listener implements EventSubscriberInterface
 {
     static public function getSubscribedEvents() {
         return array(
+            'core.user_setup'=> 'load_language_on_setup', 
             'core.page_header'  => 'add_page_header_link',
+            'core.memberlist_view_profile' => 'add_profiledata',
         );
     }
 
@@ -47,10 +49,37 @@ class main_listener implements EventSubscriberInterface
     }
 
 
+    public function load_language_on_setup($event) {
+        $lang_set_ext = $event['lang_set_ext'];
+        $lang_set_ext[] = array(
+            'ext_name' => 'unilang/languages',
+            'lang_set' => 'common',
+        );
+        $event['lang_set_ext'] = $lang_set_ext;
+    }
+
     public function add_page_header_link($event) {
-        $this->user->add_lang_ext('unilang/languages', 'common');
+        //add the page link for the language list info page
         $this->template->assign_vars(array(
             'U_LANG_INFO_PAGE'   => $this->helper->route('unilang_languages_controller'),
         ));
     }
+
+
+    public function add_profiledata($event) {
+        $member = $event['member'];
+        //add language data to the template
+        //TODO
+        //add location data to the template
+        $this->template->assign_vars(array(
+            'NATIONALITY' => $this->lf->get_country_name($member['user_nationality']),
+            'NATIONALITYICON' => $this->lf->countryicon($member['user_nationality']),
+            'COUNTRY' => $this->lf->get_country_name($member['user_country']),
+            'COUNTRYICON' => $this->lf->countryicon($member['user_country']),
+            'REGION' => $this->lf->get_region_name($member['user_country'],$member['user_region']),
+            'REGIONICON' => $this->lf->regionicon($member['user_country'],$member['user_region'])
+        ));
+    }
+
+
 }
