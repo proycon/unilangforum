e9e71718e66b971f3c8e46fca315729e72374758
Luis Romao <luisromao@gmail.com>
2017-08-08 20:20:13 +0100

    test

--- phpBB3/ext/unilang/languages/includes/lang_functions.php
+++ phpBB3/ext/unilang/languages/includes/lang_functions.php
@@ -919,7 +919,7 @@ class lang_functions {
         $min_proficiency = 0; 
 
         usort($languages, array($this,"secondarySort"));
-        print ($languages);
+        print (array_values($languages));
 
         foreach ($languages as $language) {
          if ($language['proficiency'] >= $min_proficiency) {
