abf8ee2aa08a813a2867bad30dc77b5d9fe3f657
Luis Romao <luisromao@gmail.com>
2017-08-08 18:59:47 +0100

    test

--- phpBB3/ext/unilang/languages/includes/lang_functions.php
+++ phpBB3/ext/unilang/languages/includes/lang_functions.php
@@ -52,11 +52,13 @@ class lang_functions {
 
 
     function language_names() {
-        return $this->langdata->language_names;
+        $langs = $this->langdata->language_names;
+        return asort($langs);
     }
 
     function language_names_en() {
-        return $this->langdata->language_names_en;
+        $langs = $this->langdata->language_names_en;
+        return asort($langs);
     }
 
     function country_names() {
