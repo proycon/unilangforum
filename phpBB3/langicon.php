575ee6f89a3aeff397ea7d00d04d4b82dc506fa4
Maarten van Gompel <proycon@anaproy.nl>
2017-08-15 22:58:58 +0200

    fixed noflag path

--- phpBB3/langicon.php
+++ phpBB3/langicon.php
@@ -17,11 +17,11 @@ if (isset($_GET['lang'])) {
         cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png");
     } else {
         header('Content-type: image/png');
-        cat($phpbb_root_path."images/langicons/noflag.png");
+        cat($phpbb_root_path."ext/unilang/languages/styles/all/theme/images/langicons/noflag.png");
     }
 } else {
         header('Content-type: image/png');
-        cat($phpbb_root_path."images/langicons/noflag.png");
+        cat($phpbb_root_path."ext/unilang/languages/styles/all/theme/images/langicons/noflag.png");
 }
 
 ?>
