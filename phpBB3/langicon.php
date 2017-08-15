078689e3ca3e1fc3c57bacded7bb95b3fabe0cdb
Maarten van Gompel <proycon@anaproy.nl>
2017-08-15 22:45:23 +0200

    Added new langicon.php #21

---
+++ phpBB3/langicon.php
@@ -0,0 +1,25 @@
+<?php
+$phpbb_root_path = './';
+
+function cat($file) {
+        $fp = fopen($file,'r');
+        echo fread($fp,filesize($file));
+        fclose($fp);
+}
+
+
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+    header('Content-type: image/png');
+    if (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png")) {
+        cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png");
+    } else {
+        header('Content-type: image/png');
+        cat($phpbb_root_path."images/langicons/noflag.png");
+    }
+} else {
+        header('Content-type: image/png');
+        cat($phpbb_root_path."images/langicons/noflag.png");
+}
+
+?>
