4b292ab8b250348e2a0bb8ef077954d2d480f7cc
lromao <luisromao@gmail.com>
2017-08-16 12:45:09 +0100

    Update langicon.php

--- phpBB3/langicon.php
+++ phpBB3/langicon.php
@@ -21,15 +21,14 @@ if (isset($_GET['lang'])) {
     if (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png")) {
         cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png");
     } 
-   /* else { 
-           if (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png")) {     
-                 cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png");     
-             } */
-            else {
+    elseif (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png")) {    
                  header('Content-type: image/png');
+                 cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png");     
+     }            
+    else {
+                 header('Content-type: image/png');
                  cat($phpbb_root_path."ext/unilang/languages/styles/all/theme/images/langicons/noflag.png");
-           }
-  // }
+     }
 } else {
         header('Content-type: image/png');
         cat($phpbb_root_path."ext/unilang/languages/styles/all/theme/images/langicons/noflag.png");
