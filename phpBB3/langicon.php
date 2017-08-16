bb84d60259b4b7ae948512c9acf2eaf80a3a38c3
lromao <luisromao@gmail.com>
2017-08-16 12:47:19 +0100

    Update langicon.php

--- phpBB3/langicon.php
+++ phpBB3/langicon.php
@@ -21,9 +21,9 @@ if (isset($_GET['lang'])) {
     if (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png")) {
         cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/langicons/$lang.png");
     } 
-    elseif (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png")) {    
+    elseif (file_exists($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/FR.png")) {    
                  header('Content-type: image/png');
-                 cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/$country.png");     
+                 cat($phpbb_root_path . "ext/unilang/languages/styles/all/theme/images/countryicons/FR.png");     
      }            
     else {
                  header('Content-type: image/png');
