aa70c83555984b1b688047c19faf7b376fcfdc0f
Luis Romao <luisromao@gmail.com>
2017-11-23 19:35:05 +0000

    Test

--- phpBB3/ext/unilang/languages/includes/lang_functions.php
+++ phpBB3/ext/unilang/languages/includes/lang_functions.php
@@ -538,7 +538,7 @@ class lang_functions {
 
 
 
-           //asort($this->langdata->language_names_en);
+           sortByLangNameComplex(($this->langdata->language_names_en);
            foreach ($this->langdata->language_names_en as $baselang => $value) {        
             if (($langs == false) || (in_array($baselang,$langs))) {
             if (is_array($value)) {
